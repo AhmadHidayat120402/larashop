28b5b1ac5096651380ef5e7e1c0b3d0ff2359749
Ahmadhidayat120402 <ahmadhidayat120402@gmail.com>
2023-07-14 07:15:26 +0700

    blade templet for admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::group(
+//     ['namespace' => 'admin', 'prefix'=>'admin'], function(){
+//         Route::get('/dashboard',[DashboardController::class,'index']);
+//     }
+// );
+
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard',[DashboardController::class,'index'])->name('admin');
+
+});
+
+
